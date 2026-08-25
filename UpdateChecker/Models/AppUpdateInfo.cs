ac2469ea7f188845ea872f08387ba125c3484d45
Zaidanmo <ZaidanmoMD@gmail.com>
2026-08-25 19:00:25 +0200

    "Severity" checks of the update, in reality it simply checks if there was a major update or not

--- UpdateChecker/Models/AppUpdateInfo.cs
+++ UpdateChecker/Models/AppUpdateInfo.cs
@@ -7,6 +7,11 @@ public sealed record AppUpdateInfo(
     string AvailableVersion,
     string Source)
 {
+    public bool IsMajorUpdate =>
+        GetLeadingVersionNumber(InstalledVersion) is long installedMajor &&
+        GetLeadingVersionNumber(AvailableVersion) is long availableMajor &&
+        installedMajor != availableMajor;
+
     public bool IsUpdateAvailable =>
         !string.IsNullOrWhiteSpace(AvailableVersion) &&
         !string.Equals(
@@ -14,4 +19,38 @@ public sealed record AppUpdateInfo(
             AvailableVersion,
             StringComparison.OrdinalIgnoreCase
         );
+
+    private static long? GetLeadingVersionNumber(string version)
+    {
+        ReadOnlySpan<char> value = version.AsSpan();
+        int numberStart = -1;
+
+        for (int index = 0; index < value.Length; index++)
+        {
+            if (char.IsDigit(value[index]))
+            {
+                numberStart = index;
+                break;
+            }
+        }
+
+        if (numberStart < 0)
+        {
+            return null;
+        }
+
+        int numberEnd = numberStart;
+
+        while (numberEnd < value.Length && char.IsDigit(value[numberEnd]))
+        {
+            numberEnd++;
+        }
+
+        return long.TryParse(
+            value[numberStart..numberEnd],
+            out long leadingNumber
+        )
+            ? leadingNumber
+            : null;
+    }
 }
